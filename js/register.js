5990cb3aac4fbeda533d36b7af26206c726fc67a
wellsjohn220 <60244641+wellsjohn220@users.noreply.github.com>
2023-11-09 12:24:47 +1100

    Add files via upload

--- js/register.js
+++ js/register.js
@@ -1,9 +1,10 @@
-{/* <script> */}
-  const scriptURL = 'https://script.google.com/macros/s/AKfycbxbwH6kCXKuDb-0kyGG5H8DPeK79duCnUcM-7XK5IPQyZUW6AvNUW-H1lSG_e7Yqls/exec'
+{/* <script>  */}
+  const scriptURL = 'https://script.google.com/macros/s/AKfycby69BFjuFqbLbxt7GbHsYEeVJ87daMxiSQX4Vu_kkHn2iMaAinkNO_0kdEUKz4J4qL-/exec'
   const form = document.forms['submit-to-google-sheet']
   const msg = document.getElementById("msg")  
 
-  const scriptURL2 = 'https://script.google.com/macros/s/AKfycbzTFfJJ8-6F7lwArpuDny4TmwilwbscK2crEesOxfSAsaWKRPY61Jw17T7sCPo4X5g/exec'
+  const scriptURL2 = 'https://script.google.com/macros/s/AKfycbxRFU7gJwElZci_iLIyoOL_8MXFN_MLaOcBGNPOTsGVg9b9V5bG-VsT8KpxSB05eg/exec'  
+  
   const form2 = document.forms['contact-to-google-sheet']
   const msg2 = document.getElementById("msg2")  
 
@@ -19,16 +20,17 @@
       })
       .catch(error => console.error('Error!', error.message))
   })  
+
   form2.addEventListener('submit', e => {
     e.preventDefault()
-    fetch(scriptURL2, { method: 'POST', body: new FormData(form)})
+    fetch(scriptURL2, { method: 'POST', body: new FormData(form2)})
       .then(response => {
-          msg2.innerHTML = "Thank you send contact details"
+          msg2.innerHTML = "Thanks for your contact details by 12345 John"
           setTimeout(function(){
-              msg2.innerHTML = "CONTACT"
+              msg2.innerHTML = "Contact"
           }, 5000)
           form2.reset()
       })
       .catch(error => console.error('Error!', error.message))
-  })  
+  })
 // </script>
